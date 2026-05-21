501c05dfbe47b04d04753325da93ab09f769dafc
Sid-Sutar <sidsutar120704@gmail.com>
2026-05-21 16:48:39 +0530

    Setup Professional FastAPI Architecture

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
-app = FastAPI()
+
+from app.core.config import settings
+from app.api.routes.health import router as health_router
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION
+)
+
+# Include routes
+app.include_router(health_router)
+
 @app.get("/")
-def home():
-    return{"message" : "AI Contract Risk Agent Running"}
+def root():
+    return {
+        "message": f"{settings.APP_NAME} is running"
+    }
+
